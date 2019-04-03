278a0afc805f33204cf5992397cd05cdcfde1967
Nick Gotsinas <nick.gotsinas@gmail.com>
2019-04-03 11:35:05 -0400

    add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Setting up links..."
+ln -s $HOME/.dotfiles/zshrc.linux $HOME/.zshrc
+ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/vim $HOME/.vim
+ln -s $HOME/.dotfiles/screenrc $HOME/.screenrc
+ln -s $HOME/.dotfiles/fonts $HOME/.fonts
+ln -s $HOME/.dotfiles/dircolors $HOME/.dircolors
+ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/bin $HOME/bin
+echo "Done!"
