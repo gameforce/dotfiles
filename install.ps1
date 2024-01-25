770bbd826bb87bd46fe0d31c7128dfcc76c2f457
Nick <nick.gotsinas@gmail.com>
2024-01-25 00:20:31 -0500

    Add powershell config

---
+++ install.ps1
@@ -0,0 +1,34 @@
+# temporary bypass execution policy
+# powershell.exe -noprofile -executionpolicy bypass -file .\install.ps1
+Set-ExecutionPolicy -Scope CurrentUser Bypass
+
+# update winget
+winget update
+
+# update packages
+winget upgrade --all --include-unknown
+
+# Install PowerShell, Windows Terminal, oh-my-posh
+winget install Microsoft.PowerShell
+winget install Microsoft.WindowsTerminal
+winget install JanDeDobbeleer.OhMyPosh
+
+# reload PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+# initialize oh-my-posh
+oh-my-posh --init --shell pwsh --config ~/default.omp.json | Invoke-Expression
+
+# add oh-ny-posh to $profile
+Add-Content -Path $profile "`noh-my-posh --init --shell pwsh --config ~/default.omp.json '| Invoke-Expression"
+
+# install icons
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+# add icons to $profile
+Add-Content -Path $profile "`nImport-Module -Name Terminal-Icons"
+
+# reload profile
+. $PROFILE
+
+
